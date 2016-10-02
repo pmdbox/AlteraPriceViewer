82feb60cec449ac8f03bbacdb160a688616bb6c7
Maryan Petryshyn <maryanp@ukr.net>
2016-10-02 22:34:00 +0300

    Initial commit.

---
+++ src/AlteraPriceViewer.java
@@ -0,0 +1,8 @@
+/**
+ * Created by maryan on 02.10.2016.
+ */
+public class AlteraPriceViewer {
+    public static void main(String[] args) {
+        System.out.println("Print price file to console.");
+    }
+}
