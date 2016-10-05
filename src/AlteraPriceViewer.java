c178dd5555361bd9370dfe1c548ee3229a66169d
Maryan Petryshyn <maryanp@ukr.net>
2016-10-05 14:04:51 +0300

    Move class PriceFileReader2 from another branch in master.

--- src/AlteraPriceViewer.java
+++ src/AlteraPriceViewer.java
@@ -4,6 +4,7 @@
 public class AlteraPriceViewer {
     public static void main(String[] args) {
         if (args.length>0) {
+            PriceFileReader2 price2=new PriceFileReader2(args[0]);
             PriceFileReader price=new PriceFileReader(args[0]);
             System.out.println(price.getContent());
         }
---
+++ src/PriceFileReader2.java
@@ -0,0 +1,82 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * Created by maryan on 03.10.2016.
+ */
+public class PriceFileReader2 {
+
+    private String filename;
+    private String content;
+
+    PriceFileReader2(){
+        filename = "";
+        content = "";
+    }
+
+    PriceFileReader2(String file){
+        filename = file;
+        content = "";
+        ReadFile();
+    }
+
+    public void setFilename(String file){
+        filename = file;
+        content = "";
+        ReadFile();
+    }
+
+    public String  getFilename(){
+        return filename;
+    }
+
+    private void ReadFile(){
+
+        File file=new File(filename);
+        if(file.exists()) {
+            FileInputStream inputstream = null;
+            Scanner reader=null;
+
+            try {
+                inputstream = new FileInputStream(file);
+                reader = new Scanner(inputstream, "UTF-8");
+
+                while (reader.hasNextLine()) {
+                    String line = reader.nextLine();
+                    System.out.println(line);
+                }
+            }
+            catch (FileNotFoundException exc)
+            {
+                System.out.println(exc.getMessage());
+            }
+            finally {
+                if (inputstream != null) {
+                    try{
+                        inputstream.close();
+                    }
+                    catch (IOException exc){
+                        System.out.println(exc.getMessage());
+                    }
+                }
+                if (reader != null) {
+                    reader.close();
+                }
+            }
+
+        }
+        else
+        {
+            System.out.println("Wrong filename!");
+        }
+    }
+
+    public String getContent(){
+        return content;
+    }
+
+}
