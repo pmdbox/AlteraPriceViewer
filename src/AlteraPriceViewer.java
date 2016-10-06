35390b52a980396c7e22bc9ecb0a8c49edcd570d
Maryan Petryshyn <maryanp@ukr.net>
2016-10-06 20:33:44 +0300

    AlteraPriceViewer class. Control argument.

--- src/AlteraPriceViewer.java
+++ src/AlteraPriceViewer.java
@@ -5,37 +5,32 @@
 public class AlteraPriceViewer {
     public static void main(String[] args) {
 
-        dbImport db = new dbImport();
-        db.clearTable();
+        if (args.length>0) {
+            dbImport db = new dbImport();
+            db.clearTable();
 
-        String filename = args[0];
-        String ext = "";
+            String filename = args[0];
+            String ext = "";
 
-        int i = filename.lastIndexOf('.');
-        int p = Math.max(filename.lastIndexOf('/'), filename.lastIndexOf('\\'));
+            int i = filename.lastIndexOf('.');
+            int p = Math.max(filename.lastIndexOf('/'), filename.lastIndexOf('\\'));
 
-        if (i > p) {
-            ext = filename.substring(i+1);
-        }
-        if (ext.equals("txt")){
-            PriceFileReader2 price=new PriceFileReader2(filename);
+            if (i > p) {
+                ext = filename.substring(i+1);
+            }
+            if (ext.equals("txt")){
+                PriceFileReader2 price=new PriceFileReader2(filename);
 
-            while(price.hasNextRow()){
-                System.out.print("Added "+price.getCurrentLineNum()+" lines.\r");
-                if(price.getCurrentLineNum()>6){
-                    db.addRow(price.getNextRow());
-                }
-                else{
-                    price.getNextRow();
+                while(price.hasNextRow()){
+                    System.out.print("Added "+price.getCurrentLineNum()+" lines.\r");
+                    if(price.getCurrentLineNum()>6){
+                        db.addRow(price.getNextRow());
+                    }
+                    else{
+                        price.getNextRow();
+                    }
                 }
             }
-        }
-
-
-
-
-
-        if (args.length>0) {
 /*
             String filename = args[0];
             String ext = "";
